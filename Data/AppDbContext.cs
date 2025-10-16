5bb814f7f96260db06bb5e111265454d0db0ce86
Alex <kaka_alex@inbox.ru>
2025-10-16 20:45:43 +0300

    Созданная миграция

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Web
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Usluga> Uslugi;
+    }
+}
